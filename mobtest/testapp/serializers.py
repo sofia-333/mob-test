4fb8f1fc2f1dd59f28a93997aa4c57b6c12d95d0
Sofia Muradovi <s.muradovi@mobilise.app>
2023-01-30 13:52:39 +0400

    [ADD] ContractorApplicationSerializer serializer

--- mobtest/testapp/serializers.py
+++ mobtest/testapp/serializers.py
@@ -1,4 +1,20 @@
-# Implement required serializers
+from rest_framework import serializers
 
-# class ContractorSerializer(serializers.ModelSerializer):
-#     class Meta:
+from mobtest.serializers import UserSerializer
+from testapp.models import Contractor, ContractorApplication
+
+
+class ContractorSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Contractor
+        fields = ('user', 'phone', 'nationality', 'comment', 'pay_rate')
+
+
+class ContractorApplicationSerializer(serializers.ModelSerializer):
+    contractor = ContractorSerializer()
+
+    class Meta:
+        model = ContractorApplication
+        fields = ('contractor', 'job_name', 'start_date', 'end_date')
