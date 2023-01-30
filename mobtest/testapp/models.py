b11ae8b0a054414aebb304b3defadc1864777cf3
Sofia Muradovi <s.muradovi@mobilise.app>
2023-01-30 13:50:16 +0400

    [ADD] Contractor and ContractorApplication models

--- mobtest/testapp/models.py
+++ mobtest/testapp/models.py
@@ -1,9 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
 class Contractor(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=25)
+    nationality = models.CharField(max_length=25)
+    comment = models.TextField(max_length=100, null=True, blank=True)
+    pay_rate = models.DecimalField(decimal_places=2, max_digits=10)
 
 
 class ContractorApplication(models.Model):
-    pass
+    contractor = models.ForeignKey(to=Contractor, on_delete=models.PROTECT, related_name='applications')
+    job_name = models.CharField(max_length=25)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
